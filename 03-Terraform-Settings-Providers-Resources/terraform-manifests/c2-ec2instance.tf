4b532c82ce26a5021ce10439dae98005ed8c85fd
kjm0001 <kyle.maggard@experity.com>
2022-10-15 11:07:02 -0400

    refactor: 💡 Updated for learn branch

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
@@ -1,8 +1,16 @@
+# Template
+/*
+<BLOCK TYPE> "<BLOCK LABEL>" "<BLOCK LABEL>" {
+  # Block body
+  <IDENTIFIER> = <EXPRESSION> # Argument
+}
+*/
+
 # Resource: EC2 Instance
 resource "aws_instance" "myec2vm" {
-  ami = "ami-0742b4e673072066f"
+  ami           = "ami-0742b4e673072066f"
   instance_type = "t3.micro"
-  user_data = file("${path.module}/app1-install.sh")
+  user_data     = file("${path.module}/app1-install.sh")
   tags = {
     "Name" = "EC2 Demo"
   }
