2e7ada78c8eba5cf464e0c38e569ec756adca66e
kjm0001 <kyle.maggard@experity.com>
2022-10-15 11:52:02 -0400

    refactor: 💡 added additional resource notes

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
@@ -14,7 +14,8 @@ resource "aws_instance" "myec2vm" { # aws_instance => resource type myec2vm => r
   #provider = aws.aws-west-1 # optional meta-argument 
   ami           = "ami-0742b4e673072066f"
   instance_type = "t3.micro"
-  user_data     = file("${path.module}/app1-install.sh")
+  user_data     = file("${path.module}/app1-install.sh") # allows for execution of script
+  # use tf file function
   tags = { # block inside block
     "Name" = "EC2 Demo"
   }
