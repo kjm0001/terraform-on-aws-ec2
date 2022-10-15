234bb28197c0094a30d17592c1e8644576266401
kjm0001 <kyle.maggard@experity.com>
2022-10-15 11:17:05 -0400

    refactor: 💡 added required provider notes

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
@@ -3,14 +3,17 @@ terraform {
   required_version = "~> 0.14" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
   #required_version = "~> 1.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
   required_providers {
-    aws = {
+    aws = { # local name are module specific and should be unique per module
+      # tf configuaration always refer to local nane of provider outside required_provider block
+      # any name can be used for local name
+      # best practice use preferred local name of that provider
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
   }
 }
 # Provider Block
-provider "aws" {
+provider "aws" { # aws must match name as used in required_provider name
   region = "us-east-1"
 }
 
