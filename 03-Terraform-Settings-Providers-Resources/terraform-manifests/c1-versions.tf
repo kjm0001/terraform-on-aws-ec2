282672a673d11af723ef66350950a10b1be11987
kjm0001 <kyle.maggard@experity.com>
2022-10-15 11:28:39 -0400

    refactor: 💡 added additional provider notes

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c1-versions.tf
@@ -1,13 +1,14 @@
 # Terraform Block
 terraform {
-  required_version = "~> 0.14" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
-  #required_version = "~> 1.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+  #required_version = "~> 0.14" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
+  required_version = "~> 1.0" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
   required_providers {
     aws = { # local name are module specific and should be unique per module
       # tf configuaration always refer to local nane of provider outside required_provider block
       # any name can be used for local name
       # best practice use preferred local name of that provider
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws" # source address [hostname]/<namespace>/<type> 
+      # default hostname is registry.terraform.io
       version = "~> 3.0"
     }
   }
