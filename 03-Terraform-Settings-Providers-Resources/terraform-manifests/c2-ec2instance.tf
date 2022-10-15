2858ec8922659b25f8e90cdec236b04f34cafb0a
kjm0001 <kyle.maggard@experity.com>
2022-10-15 11:39:39 -0400

    refactor: 💡 added resource block notes

--- 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
+++ 03-Terraform-Settings-Providers-Resources/terraform-manifests/c2-ec2instance.tf
@@ -2,16 +2,20 @@
 /*
 <BLOCK TYPE> "<BLOCK LABEL>" "<BLOCK LABEL>" {
   # Block body
+  #provider = aws.aws-west-1 # optional meta-argument 
   <IDENTIFIER> = <EXPRESSION> # Argument
 }
 */
 
 # Resource: EC2 Instance
-resource "aws_instance" "myec2vm" {
+resource "aws_instance" "myec2vm" { # aws_instance => resource type myec2vm => resource local name
+  # resource local name can be referenced elsewhere in same module
+  # resource type and resource local name must be unique within module
+  #provider = aws.aws-west-1 # optional meta-argument 
   ami           = "ami-0742b4e673072066f"
   instance_type = "t3.micro"
   user_data     = file("${path.module}/app1-install.sh")
-  tags = {
+  tags = { # block inside block
     "Name" = "EC2 Demo"
   }
 }
