be097fcbd21278039166a0d4c43dadc202b56571
kjm0001 <kyle.maggard@experity.com>
2022-10-16 09:39:26 -0400

    docs: ✏️ added local notes

--- 06-AWS-VPC/06-02-AWS-VPC-using-Terraform/terraform-manifests/v2-vpc-module-standardized/c3-local-values.tf
+++ 06-AWS-VPC/06-02-AWS-VPC-using-Terraform/terraform-manifests/v2-vpc-module-standardized/c3-local-values.tf
@@ -1,11 +1,12 @@
 # Define Local Values in Terraform
+# A local value assigns a name to an expression, so you can use the name multiple times within a module instead of repeating the expression.
 locals {
-  owners = var.business_divsion
+  owners      = var.business_divsion
   environment = var.environment
-  name = "${var.business_divsion}-${var.environment}"
+  name        = "${var.business_divsion}-${var.environment}"
   #name = "${local.owners}-${local.environment}"
   common_tags = {
-    owners = local.owners
+    owners      = local.owners
     environment = local.environment
   }
-} 
+}
